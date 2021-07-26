0f02d47ef91217222c14f2876b630b79695da6b4
didebbo <gianluca.napoletano.93@gmail.com>
2021-07-26 09:24:23 +0200

    refactoring sezione ps4

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var intro_video = document.getElementById("intro_video");
 var intro_label = document.getElementById("intro_label");
 var site = document.getElementById("site");
 
+// Develope Mode
+// wallpaper.style.display = "none";
+// site.style.display = "block";
+
 function play_intro() {
     wallpaper.style.display = "none";
     intro.style.display = "block";
